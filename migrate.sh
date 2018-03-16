ff199c67ceb5091c19707131b580a4299d80594e
Marian Steinbach <marian@giantswarm.io>
2018-03-16 11:54:13 +0100

    Make ELASTICSEARCH_ENDPOINT optional

--- migrate.sh
+++ migrate.sh
@@ -21,27 +21,24 @@ oldprefix="${MIGRATOR_OLD_PREFIX:-fluentd}"
 # new index name prefix
 newprefix="${MIGRATOR_NEW_PREFIX:-gslogs}"
 
-# Check env variable
-if [ "${ELASTICSEARCH_ENDPOINT}" == "" ]; then
-  echo "ERROR: Environment variable ELASTICSEARCH_ENDPOINT is not set. Exiting."
-  exit 1
-fi
+# Elasticsearch endpoint
+esendpoint="${ELASTICSEARCH_ENDPOINT:-http://elasticsearch:9200}"
 
 # Returns "200" if the index or alias exists, "404" otherwise
 function check_index_or_alias_exists {
   INDEX=$1
-  RESPONSE_STATUS=$(curl -s -o /dev/null -w "%{http_code}" ${ELASTICSEARCH_ENDPOINT}/${INDEX})
+  RESPONSE_STATUS=$(curl -s -o /dev/null -w "%{http_code}" ${esendpoint}/${INDEX})
   echo $RESPONSE_STATUS
 }
 
 # Wait for Elasticsearch to become available
 OKAY="unset"
-curl -s --head "${ELASTICSEARCH_ENDPOINT}"|grep -q "HTTP/1.1 200"
+curl -s --head "${esendpoint}"|grep -q "HTTP/1.1 200"
 OKAY=$?
 while [ "$OKAY" != "0" ]; do
   echo "Waiting for Elasticsearch to become available"
   sleep 10
-  curl -s --head "${ELASTICSEARCH_ENDPOINT}"|grep -q "HTTP/1.1 200"
+  curl -s --head "${esendpoint}"|grep -q "HTTP/1.1 200"
   OKAY=$?
 done
 
@@ -58,7 +55,7 @@ while [  $COUNTER -lt $num_days ]; do
   if [[ $INDEX_EXISTS == "200" && $ALIAS_EXISTS == "404" ]]; then
     # Create alias
     echo "Setting alias ${INDEX} pointing to index ${ALIAS}"
-    RESPONSE=$(curl -s -XPUT "${ELASTICSEARCH_ENDPOINT}/${INDEX}/_alias/{${ALIAS}}") || { echo "Creating alias failed"}
+    RESPONSE=$(curl -s -XPUT "${esendpoint}/${INDEX}/_alias/{${ALIAS}}") || { echo "Creating alias failed"}
   else
     echo "Skipping alias ${ALIAS}. Either it already exists, or index ${INDEX} does not exist."
   fi
